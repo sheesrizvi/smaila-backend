7260cf53bafe78d3407b032ca8837af878962860
tanmay8598 <tanmay8598@gmail.com>
2024-07-24 22:28:36 +0530

    notification all

--- routes/send.js
+++ routes/send.js
@@ -36,4 +36,65 @@ router.post("/send-notification", async (req, res) => {
     });
 });
 
+router.post("/send-notification-all", async (req, res) => {
+  const { title, body, image } = req.body;
+  const tokens = await User.find({ pushToken: { $exists: true } }).select(
+    "pushToken -_id"
+  );
+  const user = await User.find({ pushToken: { $exists: true } }).select("_id");
+  const registrationToken = tokens.map((item) => item.pushToken);
+  const users = user.map((item) => item._id);
+
+  const createNoti = async () => {
+    const notification = await Notification.create({
+      title,
+      description: body,
+      users,
+    });
+    if (notification) {
+      console.log("success");
+    } else {
+      res.status(404);
+      throw new Error("Error");
+    }
+  };
+
+  const message = {
+    notification: {
+      title: title,
+      body: body,
+    },
+    android: {
+      notification: {
+        imageUrl: image,
+      },
+    },
+    apns: {
+      payload: {
+        aps: {
+          "mutable-content": 1,
+        },
+      },
+      fcm_options: {
+        image: image,
+      },
+    },
+    tokens: registrationToken,
+  };
+
+  admin
+    .messaging()
+    .sendMulticast(message)
+    .then((response) => {
+      // Response is a message ID string.
+      createNoti().then(() => {
+        console.log("Successfully sent message:", response);
+        res.json("success");
+      });
+    })
+    .catch((error) => {
+      console.log("Error sending message:", error);
+    });
+});
+
 module.exports = router;
